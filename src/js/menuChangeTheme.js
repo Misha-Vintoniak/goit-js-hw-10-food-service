6e42230ae251eb3d6df5ab7b4277ba773aa20d31
Misha-Vintoniak <63545726+Misha-Vintoniak@users.noreply.github.com>
2020-11-07 12:17:43 +0200

    changes menuChangeTheme.js
    add changes in menuChangeTheme.js

--- src/js/menuChangeTheme.js
+++ src/js/menuChangeTheme.js
@@ -3,7 +3,7 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const body = document.body;
+const body = document.body.classList;
 const themeSwitchToggle = document.querySelector('.theme-switch__toggle');
 const useTheme = localStorage.getItem('theme');
 
@@ -12,20 +12,20 @@ themeSwitchToggle.addEventListener('change', onChangeTheme);
 
 function addTheme() {
   if (useTheme) {
-    document.body.classList.add(Theme.LIGHT);
+    body.add(useTheme);
+    console.log(useTheme);
   }
-  if (document.body.classList.contains(Theme.DARK)) {
+  if (body.contains(Theme.DARK)) {
     themeSwitchToggle.checked = true;
   }
 }
 
 function onChangeTheme(e) {
-  if (e.target.checked === true) {
-    document.body.classList.add(Theme.LIGHT);
-    document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+  if (e.target.checked) {
+    body.replace(Theme.LIGHT, Theme.DARK);
     localStorage.setItem('theme', Theme.DARK);
   } else {
-    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    body.replace(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
   }
 }
